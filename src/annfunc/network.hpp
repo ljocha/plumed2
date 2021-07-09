6402acba2408db98b6666a6ae8ae245d320088ad
Martin Kurecka <kur3cka.martin@gmail.com>
2021-07-09 13:13:11 +0200

    double prec; any number of layers; rename

--- src/annfunc/network.hpp
+++ src/annfunc/network.hpp
@@ -1,131 +1,119 @@
+#include <cmath>
 #include "./matrix.hpp"
 
+template <typename Scalar>
 class Network
 {
-using vector = std::vector<double>;
+using vector = std::vector<Scalar>;
+using fn_t = Scalar (*)(Scalar);
 
 private:
-    int input_size;
-    int hidden_size;
-
+    int num_layers;
     double rescale_factor;
 
-    vector input_layer;
-    vector hidden_layer;
-    double output_layer;
+    std::vector<int> layer_sizes;
 
-    matrix w_input_hidden;
-    vector w_hidden_output;
+    std::vector<matrix<Scalar>> weights;
+    std::vector<vector> biases;
+    
+    std::vector<vector> layers;
+    std::vector<vector> gradients;
 
-    vector b_hidden;
-    double b_output;
-
-    vector grad_input; // input layer: output(i) | x
-    vector grad_hidden; // hidden layer: output(i) | y
-
-    void set_weights(const vector& w_input_hidden, const vector& w_hidden_output) {
-        std::copy_n(w_input_hidden.begin(), input_size * hidden_size, this->w_input_hidden.data().begin());
-        std::copy_n(w_hidden_output.begin(), hidden_size, this->w_hidden_output.begin());
-    }
-
-    void set_biases(const vector& b_hidden, double b_output) {
-        std::copy_n(b_hidden.begin(), hidden_size, this->b_hidden.begin());
-        this->b_output = b_output;
-    }
+    std::vector<fn_t> activations;
+    std::vector<fn_t> d_activations;
 
 public:
-    explicit Network(
-        int input_size, int hidden_size, double rescale_factor,
-        vector& w_input_hidden, const vector& w_hidden_output,
-        const vector& b_hidden, double b_output
-    )
-    : input_size(input_size)
-    , hidden_size(hidden_size)
+    explicit Network(std::vector<int> layer_sizes, std::vector<fn_t> activations, std::vector<fn_t> d_activations, Scalar rescale_factor = 1)
+    : num_layers(layer_sizes.size())
     , rescale_factor(rescale_factor)
-    , input_layer(vector(input_size))
-    , hidden_layer(vector(hidden_size))
-    , w_input_hidden(matrix(hidden_size, input_size))
-    , w_hidden_output(vector(hidden_size))
-    , b_hidden(vector(hidden_size))
-    , grad_input(vector(input_size))
-    , grad_hidden(vector(hidden_size))
+    , layer_sizes(std::move(layer_sizes))
+    , weights(std::vector<matrix<Scalar>>(num_layers - 1))
+    , biases(std::vector<vector>(num_layers))
+    , layers(std::vector<vector>(num_layers))
+    , gradients(std::vector<vector>(num_layers-1))
+    , activations(std::move(activations))
+    , d_activations(std::move(d_activations))
     {
-        set_weights(w_input_hidden, w_hidden_output);
-        set_biases(b_hidden, b_output);
+        for (int i = 0; i < num_layers - 1; ++i) {
+            weights[i] = matrix<Scalar>(this->layer_sizes[i+1], this->layer_sizes[i]);
+        }
+
+        for (int i = 0; i < num_layers; ++i) {
+            biases[i] = vector(this->layer_sizes[i], 0);
+        }
+
+        for (int i = 0; i < num_layers; ++i) {
+            layers[i] = vector(this->layer_sizes[i]);
+        }
+
+        for (int i = 0; i < num_layers-1; ++i) {
+            gradients[i] = vector(this->layer_sizes[i]);
+        }
     }
 
-    double forth_and_back_propagation() {
+    void set_weights(int layer_index, const std::vector<double>& w) {
+        std::copy(w.begin(), w.end(), weights[layer_index].data().begin());
+    }
+
+    void set_biases(int layer_index, const std::vector<double>& b) {
+        std::copy(b.begin(), b.end(), biases[layer_index].begin());
+    }
+
+    vector& compute_output() {
         /* rescale the input */
-        for (double& i : input_layer ) {
-            i *= rescale_factor;
+        for (Scalar& v : layers[0] ) {
+            v *= rescale_factor;
         }
 
         /* compute innier potentials in the hidden layer */
-        hidden_layer = b_hidden;
-        w_input_hidden.add_right_vector_multiply(input_layer, hidden_layer);
-
-        /* compute
-        *  - activations in the hidden layer
-        *  - the gradient with respect to output value in the hidden layer
-        */
-        for (int k = 0; k < hidden_size; k++) {
-            hidden_layer[k] = tanh(hidden_layer[k]);
-            grad_hidden[k] = (1 - hidden_layer[k] * hidden_layer[k]) * w_hidden_output[k];
+        for (int l = 1; l < num_layers; ++l) {
+            std::copy(biases[l].begin(), biases[l].end(), layers[l].begin());
+            weights[l-1].add_right_vector_multiply(layers[l-1], layers[l]);
+            /* compute activations in the hidden layer */
+            for (size_t i = 0; i < layers[l].size(); i++) {
+                layers[l][i] = activations[l](layers[l][i]);
+            }
         }
 
-        /* compute the output value */
-        output_layer = b_output;
-        for (int k = 0; k < hidden_size; k++) {
-            output_layer += w_hidden_output[k] * hidden_layer[k];
+        return layers.back();
+    }
+
+    vector& compute_gradient(int output_index) {
+        weights.back().copy_row(output_index, gradients.back());
+        
+        for (size_t i = 0; i < gradients.back().size(); ++i) {
+            gradients.back()[i] *= d_activations[num_layers-1](layers[num_layers-1][output_index]) * d_activations[num_layers-2](layers[num_layers-2][i]);
         }
 
-        /* compute the gradient with respect to output value in the input layer */
-        w_input_hidden.left_vector_multiply(grad_hidden, grad_input);
+        for (int l = num_layers - 3; l >= 0; --l) {
+            weights[l].left_vector_multiply(gradients[l+1], gradients[l]);
+            
+            for (size_t i = 0; i < gradients[l].size(); ++i) {
+                gradients[l][i] *= d_activations[l](layers[l][i]);
+            }
+        }
 
         /* rescale the input gradient */
-        for (double& g : grad_input) {
+        for (Scalar& g : gradients.front()) {
             g *= rescale_factor;
         }
-        
-        return output_layer;
-    }
 
-    void print_params(std::ostream& out) {
-        out << "weights0:\n";
-        w_input_hidden.print(out);
-        out << "weights1:\n";
-        for (int i = 0; i < hidden_size; i++) {
-            out << w_hidden_output[i] << " ";
-        }
-        out << "biases0:\n";
-        for (int i = 0; i < hidden_size; i++) {
-            out << b_hidden[i] << " ";
-        }
-        out << "\nbiases1:\n";
-        out << b_output << " ";
-        out << "\n";
-    }
-
-    void print_output(std::ostream& out) {
-        out << "input layer: \n";
-        for (int i = 0; i < input_size; i++) {
-            out << input_layer[i] << " ";
-        }
-        out << "\n\nhidden layer: \n";
-        for (int i = 0; i < hidden_size; i++) {
-            out << hidden_layer[i] << " ";
-        }
-        out << "\n\nouput layer: ";
-        out << output_layer << " ";
-        out << "\n";
-        out << "\n";
+        return gradients.front();
     }
 
     vector& get_input() {
-        return input_layer;
+        return layers.front();
     }
 
-    vector& get_input_gradient() {
-        return grad_input;
-    }
+    static Scalar tanh(Scalar v) {return std::tanh(v); }
+    static Scalar d_tanh(Scalar fv) { return 1 - fv*fv; }
+
+    static Scalar sigmoid(Scalar v) { return 1/(1 + exp(v)); }
+    static Scalar d_sigmoid(Scalar fv) { return (1 - fv) * fv; }
+
+    static Scalar linear(Scalar v) { return v; }
+    static Scalar d_linear(Scalar) { return 1; }
+
+    static Scalar relu(Scalar v) { return v < 0 ? 0 : v; }
+    static Scalar d_relu(Scalar fv) { return fv <= 0 ? 0 : 1; }
 };
